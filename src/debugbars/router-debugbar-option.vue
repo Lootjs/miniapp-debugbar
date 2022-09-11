31ade9481a701242c3535b6508a550c1cce78a94
Loot Nurakhmetov <nurakhmetov.l@rahmetapp.kz>
2022-09-11 23:47:36 +0600

    added axios + route debugbar rewrited to options api

--- src/debugbars/router-debugbar-option.vue
+++ src/debugbars/router-debugbar-option.vue
@@ -1,46 +1,44 @@
 <template>
-    <div class="webview-router">
-      <div class="miniapp-option__name">Router</div>
-      <pre>{{ routeData }}</pre>
-      <div class="miniapp-option__group">
-        <div class="miniapp-option__heading">Redirect</div>
-        <div class="miniapp-option__input">
-          <input type="text" v-model="url" placeholder="Enter URL">
-        </div>
-        <button @click="handleRedirect" class="miniapp-option__button">Go</button>
+  <div class="webview-router">
+    <div class="miniapp-option__name">Router</div>
+    <pre>{{ routeData }}</pre>
+    <div class="miniapp-option__group">
+      <div class="miniapp-option__heading">Redirect</div>
+      <div class="miniapp-option__input">
+        <input type="text" v-model="url" placeholder="Enter URL">
       </div>
+      <button @click="handleRedirect" class="miniapp-option__button">Go</button>
     </div>
+  </div>
 </template>
 
 <script>
-    import { useRoute, useRouter } from "vue-router";
-    import { ref } from "vue";
-
-    export default {
-        setup() {
-          const url = ref('');
-          const route = useRoute();
-          const router = useRouter();
-          const routeData = {
-            name: route.name,
-            hash: route.hash,
-            meta: route.meta,
-            fullPath: route.fullPath,
-            params: route.params,
-            query: route.query,
-          }
-          const handleRedirect = function () {
-            const parsedUrl = new URL(url.value);
-            const parsedCurrentUrl = new URL(window.location.href);
-
-            if (parsedUrl.host === parsedCurrentUrl.host) {
-              return router.push(url.value);
-            }
-
-            window.location = url.value;
-          };
-
-          return { routeData, url, handleRedirect };
-        }
+export default {
+  data: () => ({
+    routeData: {},
+    url: '',
+  }),
+  mounted() {
+    this.routeData = {
+      name: this.$route.name,
+      hash: this.$route.hash,
+      meta: this.$route.meta,
+      fullPath: this.$route.fullPath,
+      params: this.$route.params,
+      query: this.$route.query,
     }
+  },
+  methods: {
+    handleRedirect() {
+      const parsedUrl = new URL(this.url);
+      const parsedCurrentUrl = new URL(window.location.href);
+
+      if (parsedUrl.host === parsedCurrentUrl.host) {
+        return this.$router.push(this.url);
+      }
+
+      window.location = this.url;
+    }
+  }
+}
 </script>
