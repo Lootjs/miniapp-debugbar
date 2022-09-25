dc5fcad4a62a090f6e1d1621fa7aeb27f622ff52
Loot Nurakhmetov <nurakhmetov.l@rahmetapp.kz>
2022-09-25 13:42:09 +0600

    added route picker feature

--- src/debugbars/router-debugbar-option.vue
+++ src/debugbars/router-debugbar-option.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="webview-router">
+  <div class="miniapp-option">
     <div class="miniapp-option__name">Router</div>
-    <pre>{{ routeData }}</pre>
+    <div class="miniapp-option__group">
+      <pre>{{ routeData }}</pre>
+    </div>
     <div class="miniapp-option__group">
       <div class="miniapp-option__heading">Redirect</div>
       <div class="miniapp-option__input">
-        <input type="text" v-model="url" placeholder="Enter URL">
+        <input type="text" v-model="url" @input="event => setURL(event.target.value)" placeholder="Enter URL">
+      </div>
+      <div class="miniapp-option__heading">Or pick route:</div>
+      <div class="miniapp-option__input">
+        <select @change="event => setURL(event.target.value)">
+          <option v-for="route in routes" :key="route.name" :value="route.path">
+            {{ route.path }}
+          </option>
+        </select>
       </div>
       <button @click="handleRedirect" class="miniapp-option__button">Go</button>
     </div>
@@ -15,21 +25,30 @@
 <script>
 export default {
   data: () => ({
-    routeData: {},
+    routes: [],
     url: '',
   }),
   mounted() {
-    this.routeData = {
-      name: this.$route.name,
-      hash: this.$route.hash,
-      meta: this.$route.meta,
-      fullPath: this.$route.fullPath,
-      params: this.$route.params,
-      query: this.$route.query,
+    this.routes = this.$router.getRoutes();
+  },
+  computed: {
+    routeData() {
+      return {
+        name: this.$route.name,
+        hash: this.$route.hash,
+        meta: this.$route.meta,
+        fullPath: this.$route.fullPath,
+        params: this.$route.params,
+        query: this.$route.query,
+      }
     }
   },
   methods: {
     handleRedirect() {
+      if (this.url.indexOf('http') === -1) {
+        return this.$router.push(this.url);
+      }
+
       const parsedUrl = new URL(this.url);
       const parsedCurrentUrl = new URL(window.location.href);
 
@@ -38,6 +57,9 @@ export default {
       }
 
       window.location = this.url;
+    },
+    setURL(value) {
+      this.url = value;
     }
   }
 }
